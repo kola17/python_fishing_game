9ecd29cae8e1e081078860c89728924cd78c3985
kola17 <joe.ponda17@gmail.com>
2025-07-19 12:26:09 -0500

    dicking around

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from tkinter import *
 from tkinter import ttk
+import json
+
+# Loads game data from .json file
+with open('fishing_data.json', 'r') as f:
+    game_data = json.load(f)
+
+# Location data Variable
+location = game_data["locations"]
+
+# Fish Species Variable
+fish_species = game_data["fish_species"]
 
 # Creates Tkinter Window
 root = Tk()
@@ -11,9 +22,10 @@ frame.grid(column=3, row=3, sticky=(N,W,E,S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-#Creates a dropdown menu for selecting a location
+# Creates a dropdown menu for selecting a location
 
 
 root.mainloop()
 
-
+print(location.items())
+print(fish_species.items())
