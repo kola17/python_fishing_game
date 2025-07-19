4262b05a07fe2a905377d368e21044808ede07da
kola17 <joe.ponda17@gmail.com>
2025-07-19 03:38:28 -0500

    Added one location and two fish

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from tkinter import ttk
 root = Tk()
 root.title("Cool Fishing :)")
 
+# Creates frame with 3 columns and 3 rows
 frame = ttk.Frame(root, padding=1, borderwidth=5, relief=SUNKEN, width=250, height=250)
 frame.grid(column=3, row=3, sticky=(N,W,E,S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-fish_button = ttk.Button(frame, text='Cast!').grid(row=2, column=2)
-stop_button = ttk.Button(frame, text='Stop!').grid(row=0, column=0)
+#Creates a dropdown menu for selecting a location
+
+
 root.mainloop()
 
 
