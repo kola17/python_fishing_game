2c73e997ff16534bdfece9674749b088cf73f5d7
Joe <joe.ponda17@gmail.com>
2025-07-21 21:22:06 -0500

    Added Fish Classi

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from tkinter import ttk
 root = Tk()
 root.title("Cool Fishing :)")
 
+# Creates Tkinter Frame
 frame = ttk.Frame(root, padding=1, borderwidth=5, relief=SUNKEN, width=250, height=250)
 frame.grid(column=3, row=3, sticky=(N,W,E,S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+# Creates Tkinter Buttons
 fish_button = ttk.Button(frame, text='Cast!').grid(row=2, column=2)
 stop_button = ttk.Button(frame, text='Stop!').grid(row=0, column=0)
+
+# Runs Tkinter Window
 root.mainloop()
 
 
